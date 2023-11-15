2e5b8af726d8bbf90ff7bb6a15eaf1afd28cd5e9
ayberkkk <ayberktiglii@gmail.com>
2023-11-15 10:55:01 +0300

    :)

--- utils/const.jsx
+++ utils/const.jsx
@@ -38,6 +38,38 @@ export const useMember = () => {
     handleLogout,
   };
 };
+export const addToCart = (id, productDetail, quantity) => {
+  const { cart, addToCart: updateCartInContext } = useCart();
+  if (quantity === 0) {
+    Swal.fire({
+      title: "Product Not Added to Cart",
+      text: "The quantity is 0, so the product cannot be added to the cart.",
+      icon: "error",
+    });
+  } else {
+    const existingItemIndex = cart.findIndex((item) => item.id === id);
+    if (existingItemIndex !== -1) {
+      const updatedCart = [...cart];
+      updatedCart[existingItemIndex].quantity += quantity;
+      updateCartInContext(updatedCart);
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
+    } else {
+      const newItem = {
+        id,
+        title: productDetail.title,
+        quantity,
+      };
+      const updatedCart = [...cart, newItem];
+      updateCartInContext(updatedCart);
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
+    }
+    Swal.fire({
+      title: "Product Added to Cart",
+      html: `<b>${productDetail.title}</b> product has been added to your cart with a quantity of <b>${quantity}</b>.`,
+      icon: "success",
+    });
+  }
+};
 export const adminMenu = [
   {
     path: "/admin",
