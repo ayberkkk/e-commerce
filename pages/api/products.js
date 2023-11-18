0690d88d0268a5a32f8b8b43d6c2c8b3cf341d50
ayberkkk <ayberktiglii@gmail.com>
2023-11-18 14:50:55 +0300

    productcard comp & delete admin api conf

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,11 +1,7 @@
-import { useState, useEffect } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { FcCompactCamera } from "react-icons/fc";
-import StarRating from "@/components/Star";
-import FavoriteButton from "@/components/FavoriteButton";
+import React, { useState, useEffect } from "react";
+import ProductCard from "@/components/ProductCard";
 
-export default function ProductList() {
+const ProductList = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -23,56 +19,10 @@ export default function ProductList() {
   return (
     <>
       {products.map((product) => (
-        <div
-          className="border border-gray-200 rounded-lg p-3 lg:m-3 m-1 lg:px-4 relative z-50 shadow-sm transition-shadow ease-in hover:shadow-lg"
-          key={product.id}
-        >
-          <div className="relative z-50">
-            <Link href={`/products/${product.id}`}>
-              <Image
-                className="object-contain lg:h-72 h-52 w-96 mb-5 transition-all ease-in hover:scale-105"
-                src={product.image}
-                width={600}
-                height={400}
-                title={product.title}
-                alt={product.title}
-              />
-              <div className="absolute left-1 lg:top-2 -top-3 z-20">
-                <Image
-                  className="w-12 h-auto object-cover p-1 ml-1"
-                  src="/logo.png"
-                  width={40}
-                  height={40}
-                />
-              </div>
-            </Link>
-            <div className="absolute right-1 lg:top-2 -top-2 z-20 cursor-pointer group">
-              <FavoriteButton
-                productId={product.id}
-              />
-            </div>
-            <div className="relative z-50 border-t-2 w-full pt-3 pb-3">
-              <h3 className="lg:text-base font-normal">
-                <span className="font-bold uppercase mr-1">
-                  {product.category}
-                </span>
-                {product.title}
-              </h3>
-              <div className="flex gap-2 mt-2 mb-2">
-                <StarRating rating={product.rating.rate} />
-                <span className="text-gray-400 text-xs">
-                  ({product.rating.count})
-                </span>
-                <FcCompactCamera />
-              </div>
-              <div className="text-green-500 font-bold text-3xl mt-2 mb-2">
-                {product.price}
-                <span className="text-lg ml-1">$</span>
-              </div>
-            </div>
-          </div>
-        </div>
+        <ProductCard key={product.id} product={product} />
       ))}
     </>
   );
-}
+};
+
+export default ProductList;
--- pages/category/[...id].js
+++ pages/category/[...id].js
@@ -1,11 +1,7 @@
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
 import Header from "@/layouts/Header";
-import StarRating from "@/components/Star";
-import { FcCompactCamera } from "react-icons/fc";
-import FavoriteButton from "@/components/FavoriteButton";
+import ProductCard from "@/components/ProductCard";
 
 function CategoryPage() {
   const router = useRouter();
@@ -31,44 +27,9 @@ function CategoryPage() {
     <>
       <Header />
       <div className="container mx-auto">
-        <ul className=" grid lg:grid-cols-4 grid-cols-2 gap-3">
+        <ul className="grid lg:grid-cols-4 grid-cols-2 gap-3">
           {categoryData.map((product) => (
-            <>
-              <div className="border border-gray-200 rounded-lg p-3 lg:m-3 m-1 lg:px-4 relative z-50">
-                <Link href={`/products/${product.id}`}>
-                  <Image
-                    className="object-contain lg:h-72 h-52 w-96 mb-5 transition-all hover:scale-105"
-                    src={product.image}
-                    width={600}
-                    height={400}
-                    title={product.title}
-                    alt={product.title}
-                  />
-                </Link>
-                <div className="absolute right-1 top-2 z-20 cursor-pointer group">
-                  <FavoriteButton productId={product.id} />
-                </div>
-                <div className="relative z-50 border-t-2 w-full pt-3 pb-3">
-                  <h3 className="lg:text-base font-normal">
-                    <span className="font-bold uppercase mr-1">
-                      {product.category}
-                    </span>
-                    {product.title.slice(0, 20)}
-                  </h3>
-                  <div className="flex gap-2 mt-2 mb-2">
-                    <StarRating rating={product.rating.rate} />
-                    <span className="text-gray-400 text-xs">
-                      ({product.rating.count})
-                    </span>
-                    <FcCompactCamera />
-                  </div>
-                  <div className="text-green-500 font-bold text-3xl mt-2 mb-2">
-                    {product.price}
-                    <span className="text-lg ml-1">$</span>
-                  </div>
-                </div>
-              </div>
-            </>
+            <ProductCard key={product.id} product={product} />
           ))}
         </ul>
       </div>
